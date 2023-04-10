37414f4f95183db4812be7094be14ec3b8fc125e
xuannghia <nghiagod121122@gmail.com>
2023-04-11 00:54:00 +0700

    selection sort

--- sort/src/basic_sort/SelectionSort.java
+++ sort/src/basic_sort/SelectionSort.java
@@ -1,8 +1,28 @@
 package basic_sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     public static void main(String[] args) {
+        int[] array = {3, 6, 1, 7, 9, 15, 4};
+        System.out.println(Arrays.toString(selectionSort(array)));
+    }
 
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+        return array;
     }
 }
